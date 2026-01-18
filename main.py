aa45e923a11d5be50bea6c61db8edac4c693fd9a
Madhav Kandukuri <madhav165@gmail.com>
2026-01-18 17:20:58 +0530

    speed up with lower nfe_step

--- main.py
+++ main.py
@@ -1,32 +1,3 @@
-# **IndicF5: High-Quality Text-to-Speech for Indian Languages**
-
-[![Hugging Face](https://img.shields.io/badge/HuggingFace-Model-orange)](https://huggingface.co/ai4bharat/IndicF5)
-
-
-We release **IndicF5**, a **near-human polyglot** **Text-to-Speech (TTS)** model trained on **1417 hours** of high-quality speech from **[Rasa](https://huggingface.co/datasets/ai4bharat/Rasa), [IndicTTS](https://www.iitm.ac.in/donlab/indictts/database), [LIMMITS](https://sites.google.com/view/limmits24/), and [IndicVoices-R](https://huggingface.co/datasets/ai4bharat/indicvoices_r)**.  
-
-IndicF5 supports **11 Indian languages**:  
-**Assamese, Bengali, Gujarati, Hindi, Kannada, Malayalam, Marathi, Odia, Punjabi, Tamil, Telugu.**  
-
----
-
-## 🚀 Installation
-```bash
-conda create -n indicf5 python=3.10 -y
-conda activate indicf5
-pip install git+https://github.com/ai4bharat/IndicF5.git
-```
-
-
-## 🎙 Usage
-
-To generate speech, you need to provide **three inputs**:
-1. **Text to synthesize** – The content you want the model to speak.
-2. **A reference prompt audio** – An example speech clip that guides the model’s prosody and speaker characteristics.
-3. **Text spoken in the reference prompt audio** – The transcript of the reference prompt audio.
-
-
-```python
 import os
 import torch
 import numpy as np
@@ -109,22 +80,3 @@ if audio.dtype == np.int16:
 os.makedirs("samples", exist_ok=True)
 sf.write("samples/namaste.wav", np.array(audio, dtype=np.float32), samplerate=24000)
 print("Audio saved to samples/namaste.wav")
-```
-
-## References
-
-We would like to extend our gratitude to the authors of  **[F5-TTS](https://github.com/SWivid/F5-TTS)** for their invaluable contributions and inspiration to this work. Their efforts have played a crucial role in advancing  the field of text-to-speech synthesis.
-
-
-## 📖 Citation
-If you use **IndicF5** in your research or projects, please consider citing it:
-
-### 🔹 BibTeX
-```bibtex
-@misc{AI4Bharat_IndicF5_2025,
-  author       = {Praveen S V and Srija Anand and Soma Siddhartha and Mitesh M. Khapra},
-  title        = {IndicF5: High-Quality Text-to-Speech for Indian Languages},
-  year         = {2025},
-  url          = {https://github.com/AI4Bharat/IndicF5},
-}
-
